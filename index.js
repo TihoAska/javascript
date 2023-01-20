24bc238522ad64cfe793af9b02ae31bbc949bca3
TihoAska <tihomirascic18@gmail.com>
2023-01-20 18:37:11 +0100

    for petlja i objekti

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
-var button = document.getElementById("firstButton");
-var button2 = document.getElementById("secondButton");
-var lel = document.getElementById("name");
+// function mojaFunkcija(){
 
-button.addEventListener("click", funkcija);    
-button2.addEventListener("click", drugaFunkcija);
+//     var polje = [1.2, '13', 55, 22, 1];  //skup istih tipova
 
+//     for(var i = 0; i<polje.length; i++){
+//         console.log(polje[i]);
+//     }
+// }
 
-function funkcija(){
-    lel.innerHTML = prompt();
-}
+// mojaFunkcija();
 
-function drugaFunkcija(){
-    console.log("Stisni prvi button");
-}
+// var n = prompt();
+
+// function ispisiSveDoOvogBroja(n){
+//     for(var i = 0; i<=n; i++){
+//         console.log(i);
+//     }
+// }
+
+// ispisiSveDoOvogBroja(n);
+
+var osoba = { //key value parovi
+    ime: "Pero",
+    prezime: "Peric",
+    godine: 25,
+};  //skup razlicitih tipova
+
+var osoba2 = { //key value parovi
+    ime: "Ivo",
+    prezime: "Ivic",
+    godine: 50,
+}; 
+
+var zivotinja = { //key value parovi
+    vrsta: "Pas",
+    ime: "Albert",
+    godine: 8,
+}; 
+
+console.log("Ime: " + osoba.ime);
+console.log("Prezime: " + osoba.prezime);
+console.log("Godine: " + osoba.godine);
+
+console.log("Ime: " + osoba2.ime);
+console.log("Prezime: " + osoba2.prezime);
+console.log("Godine: " + osoba2.godine);
+
+console.log("Ja sam " + zivotinja.vrsta + ", zovem se " + zivotinja.ime + " i imam " + zivotinja.godine + " godina");
